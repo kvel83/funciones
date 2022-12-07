5b7b61ac52183971f9c2591a481b5945649b5b35
Katia Velasquez <katia.velasquez@.com>
2022-12-07 20:04:08 -0300

    Tercer commit: Actualizacion script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-function pintar(ele,color = 'green'){
-    ele.style.backgroundColor = color;
-    };
+const pintar = (ele,color = 'green') => ele.style.backgroundColor = color;
 ele = document.getElementById("ele1");
 ele.addEventListener("click", function(){pintar(ele,'yellow')});
